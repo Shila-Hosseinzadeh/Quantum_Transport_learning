10b884717a05054c9612b2f1504f7c4987517e9b
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-06-07 16:14:26 +0330

    Implement Hamiltonian 2-state system analysis
    Added Hamiltonian matrices and computed their eigenvalues and eigenvectors.

--- week01/day02_linear_algebra/day02_hamiltonian/hamiltonian_2state.py
+++ week01/day02_linear_algebra/day02_hamiltonian/hamiltonian_2state.py
@@ -1,1 +1,19 @@
+import numpy as np
+
+H_1 = np.array([[1,0.2],[2,2]])
+H_2 = np.array([[1, 0.8],
+              [0.8, 2]])
+eigenvalues_1 ,eigenvectors_1 = np.linalg.eigh(H_1)
+eigenvalues_2 ,eigenvectors_2 = np.linalg.eigh(H_2)
+
+print("H_1 :",H_1)
+print(" \nH_1 Eigenvalues:", eigenvalues_1)
+
+print("\n H_1 Eigenvectors:", eigenvectors_1)
+
+print("\nH_2 :",H_2)
+
+print(" \n H_2 Eigenvalues:", eigenvalues_2)
+
+print("\n H_2 Eigenvectors:", eigenvectors_2)
 
