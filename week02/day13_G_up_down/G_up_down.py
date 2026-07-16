56af5d6cfd3dd5a7e6fffc210068e8754128f8ee
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-07-17 00:44:01 +0330

    Update G_up_down.py

--- week02/day13_G_up_down/G_up_down.py
+++ week02/day13_G_up_down/G_up_down.py
@@ -13,9 +13,22 @@ G_down = (e**2/h) * T_down
 
 G_total = G_up + G_down
 
+G_P = 1.0
+G_AP = 0.3
+
+
+
+I_P = G_P * V
+I_AP = G_AP * V
+
+
+
 I = G_total * V
 
 print("G_up =", G_up)
 print("G_down =", G_down)
 print("G_total =", G_total)
 print("Current =", I)
+print("TMR =", (G_P - G_AP)/G_AP)
+print("I_P =", I_P)
+print("I_AP =", I_AP)
