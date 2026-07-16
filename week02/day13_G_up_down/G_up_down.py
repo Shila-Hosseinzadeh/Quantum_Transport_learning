1fc5fe27f7c6290bd767a6c708aa20045796fdbc
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-07-17 00:32:49 +0330

    Update G_up_down.py

--- week02/day13_G_up_down/G_up_down.py
+++ week02/day13_G_up_down/G_up_down.py
@@ -6,11 +6,16 @@ h = 6.626e-34
 T_up = 0.8
 T_down = 0.2
 
+V = 0.1
+
 G_up = (e**2/h) * T_up
 G_down = (e**2/h) * T_down
 
 G_total = G_up + G_down
 
+I = G_total * V
+
 print("G_up =", G_up)
 print("G_down =", G_down)
 print("G_total =", G_total)
+print("Current =", I)
