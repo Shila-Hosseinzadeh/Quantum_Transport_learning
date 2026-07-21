b8d9090ba0746654758340086754a938b8e19952
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-07-21 13:21:44 +0330

    Create T_vs_Bias.py

---
+++ week03/day16_T_vs_Bias/T_vs_Bias.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+E = np.linspace(-1,1,1000)
+
+T = np.exp(-E**2)
+
+EF_left = 0.2
+EF_right = -0.2
+
+mask = (E>=EF_right) & (E<=EF_left)
+
+Current = np.trapz(T[mask],E[mask])
+
+print(Current)
