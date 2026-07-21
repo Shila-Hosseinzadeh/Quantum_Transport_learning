a6671902eaef1a1ce94b86bcbe69c781afa8de03
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-07-21 13:25:41 +0330

    Update T_vs_Bias.py

--- week03/day16_T_vs_Bias/T_vs_Bias.py
+++ week03/day16_T_vs_Bias/T_vs_Bias.py
@@ -8,7 +8,7 @@ T = np.exp(-E**2)
 EF_left = 0.2
 EF_right = -0.2
 
-mask = (E>=EF_right) & (E<=EF_left)
+mask = (E>=EF_right) & (E<=EF_left)  #Energies only between Bias Window
 
 Current = np.trapz(T[mask],E[mask])
 
